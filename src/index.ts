d4535d10997e2d58aa85e326facd8930dcf18dfb
thrazznos <nsargent.ice@gmail.com>
2023-08-31 23:51:43 -0700

    setting up observable example

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
-console.log("Hello World!")
+import { Observable } from "rxjs";
+
+let observable$ = new Observable((subscriber) => {
+    subscriber.next("Hello world")
+    subscriber.complete();
+    subscriber.error();
+
+    return () => {
+        console.log("unsubscribe")
+    }
+})
+
+const observer = {
+    next: (value: any) => {
+        console.log(value);
+    },
+    complete: () => {
+        console.log("complete called")
+    },
+    error: (err: any) => {
+        console.error("shits broke")
+    }
+}
+
+const observable = observable$.subscribe(observer);
+
+observable.unsubscribe();
